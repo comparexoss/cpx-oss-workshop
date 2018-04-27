2955537cbaf5f1c34d1cf559361954fe16d8a79c
muammerbenzes <muammer@muammerbenzes.com>
2018-04-27 11:01:32 +0300

    Update Footer.vue
    change footer

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Blackbelt Team
+        Microsoft Turkey OSS Team
       </div>
       <div class="col-lg-6">
       </div>
