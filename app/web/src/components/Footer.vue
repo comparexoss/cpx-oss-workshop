88233ff6f6021690108423d2beca62a7e413e1ae
comparexoss <45731819+comparexoss@users.noreply.github.com>
2018-12-10 17:55:28 +0300

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -10,6 +10,11 @@
         <img src="/static/img/msft_logo.png">
       </div>
     </div>
+        <div class="row at-row flex-center flex-middle">
+      <div class="col-lg-24">
+        <img src="/static/img/cpx_logo.png">
+      </div>
+    </div>
     <div class="row at-row flex-center flex-middle">
       <div class="col-lg-6">
       </div>
