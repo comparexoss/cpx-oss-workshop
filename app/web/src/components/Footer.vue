d3490a9077602cbda5836e54d8d3e31321ed763c
comparexoss <45731819+comparexoss@users.noreply.github.com>
2018-12-11 16:35:14 +0300

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -52,8 +52,8 @@
    <div class="row at-row flex-center flex-middle">
       <div class="col-lg-24">
         <h1>
-          <!-- modifying code here -->
-          </h1>
+          <!-- aasdmodifying code here -->
+        </h1>
       </div>
    </div>
   </section>
